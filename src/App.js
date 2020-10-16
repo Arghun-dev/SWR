fea96a38bcdad7aff433be6c62f2a4010b3f07eb
Arghun-dev <arghun.developer@gmail.com>
2020-10-16 23:28:38 +0330

    swr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import useSWR, { SWRConfig } from 'swr'
 
 function App() {
+  const url = 'https://jsonplaceholder.typicode.com/posts'
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const { data, error } = useSWR(url, fetcher)
+
+  if (error) {
+    return <div>Error</div>
+  }
+
+  if (!data) {
+    return <div>loading...</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <pre className="App">
+      {JSON.stringify(data, null, 2)}
+    </pre>
   );
 }
 
